d7833651e9cfa2139cc48d6a15ec8bcf1c20dede
adivigo <74842803+adivigo@users.noreply.github.com>
2022-12-11 20:13:49 +0700

    Add files via upload

---
+++ task_javascript_introduction_1.js
@@ -0,0 +1,30 @@
+const biodata = {
+    name: 'Adivigo Khalishtama',
+    age: 26,
+    hobbies: ['futsal', 'game', 'flag football'],
+    isMarried: false,
+    schoolList: [
+        {   name: 'Universitas Gunadarma',
+            yearIn: '2014',
+            yearOut: '2019',
+            Major: 'teknik Informatika'
+        },
+        {
+            name: 'SMAN 11 Bandung',
+            yearIn: '2011',
+            yearOut: '2014',
+            Major: 'IPA'
+        }
+    ],
+    skills: [
+        {
+            skillName: 'coding',
+            level: 'Beginner'
+        },
+        {
+            skillName: 'public speaking',
+            level: 'Beginner'
+        }
+    ],
+    interestInCoding: true
+}
